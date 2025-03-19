2278e63651b74b0c880149e4df4db849c8ca92a9
aureliusn12 <163034788+aureliusn12@users.noreply.github.com>
2025-03-18 21:51:31 -0300

    started learning java script

---
+++ third class/script.js
@@ -0,0 +1,43 @@
+/*
+let turma = 7;
+turma = "shaw";
+
+console.log(turma);
+*/
+
+/*
+let num1 = 3;
+let num2 = "3";
+
+if(num1 === num2){
+    console.log("Iguais");
+}    else{
+        console.log("Diferentes");
+    }
+*/
+
+/*
+const turno = "noite";
+turno = "tarde";
+
+console.log(turno);
+*/
+
+
+const pessoa = {
+    nome: "Alice",
+    idade: 30,
+    casada: null // or true
+}
+
+pessoa.nome = "Bruno";
+
+const pessoa2 = {
+    nome: "Josefina",
+    idade: 30,
+    casada: true
+}
+
+console.log(pessoa.nome);
+
+console.log(pessoa2.casada);
